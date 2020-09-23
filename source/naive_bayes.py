f86ccbc7497e20b7c874352b7670d15076e16168
unknown <mthlima@poli.ufrj.br>
2020-09-23 18:18:04 -0300

    Fit Naive Bayes model

--- source/naive_bayes.py
+++ source/naive_bayes.py
@@ -26,5 +26,12 @@ predictors[:, 18] = labelencoder.fit_transform(predictors[:, 18])
 predictors[:, 19] = labelencoder.fit_transform(predictors[:, 19])
 
 
+# Fit naive bayes model
+X_train, X_test, y_train, y_test = train_test_split(predictors,
+                                                    group,
+                                                    test_size = 0.3,
+                                                    random_state = 0)
+naive_bayes = GaussianNB()
+naive_bayes.fit(X_train, y_train)
 
 
