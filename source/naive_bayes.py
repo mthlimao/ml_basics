7bfeb456027a67a53a17b5a0296768d224e2f743
unknown <mthlima@poli.ufrj.br>
2020-09-23 18:15:53 -0300

    Encode non numeric input labels

--- source/naive_bayes.py
+++ source/naive_bayes.py
@@ -9,4 +9,22 @@ credit = pd.read_csv('Credit.csv')
 predictors = credit.iloc[:,0:20].values
 group = credit.iloc[:,20].values
 
+# Convert non numeric inputs to numeric inputs by LabelEncoder
+labelencoder = LabelEncoder()
+predictors[:,0] = labelencoder.fit_transform(predictors[:,0])
+predictors[:,2] = labelencoder.fit_transform(predictors[:,2])
+predictors[:, 3] = labelencoder.fit_transform(predictors[:, 3])
+predictors[:, 5] = labelencoder.fit_transform(predictors[:, 5])
+predictors[:, 6] = labelencoder.fit_transform(predictors[:, 6])
+predictors[:, 8] = labelencoder.fit_transform(predictors[:, 8])
+predictors[:, 9] = labelencoder.fit_transform(predictors[:, 9])
+predictors[:, 11] = labelencoder.fit_transform(predictors[:, 11])
+predictors[:, 13] = labelencoder.fit_transform(predictors[:, 13])
+predictors[:, 14] = labelencoder.fit_transform(predictors[:, 14])
+predictors[:, 16] = labelencoder.fit_transform(predictors[:, 16])
+predictors[:, 18] = labelencoder.fit_transform(predictors[:, 18])
+predictors[:, 19] = labelencoder.fit_transform(predictors[:, 19])
+
+
+
 
