ec12da539ca6f03f0dcd610cc8dad59318c7ea43
unknown <mthlima@poli.ufrj.br>
2020-09-25 10:29:50 -0300

    Creat clusters with KMeans

--- source/kmeans.py
+++ source/kmeans.py
@@ -8,3 +8,7 @@ from sklearn.cluster import KMeans
 # Load iris dataset, and return unique targets and their quantities
 iris = datasets.load_iris()
 unique, quantity = np.unique(iris.target, return_counts = True)
+
+# Creat clusters with KMeans
+cluster = KMeans(n_clusters = 3)
+cluster.fit(iris.data)
