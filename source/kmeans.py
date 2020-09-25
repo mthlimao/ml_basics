2882639319845dc0490233b63ce980c51f4c9410
unknown <mthlima@poli.ufrj.br>
2020-09-25 10:28:28 -0300

    Load iris dataset

---
+++ source/kmeans.py
@@ -0,0 +1,10 @@
+from sklearn import datasets
+import numpy as np
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
+
+# Load iris dataset, and return unique targets and their quantities
+iris = datasets.load_iris()
+unique, quantity = np.unique(iris.target, return_counts = True)
