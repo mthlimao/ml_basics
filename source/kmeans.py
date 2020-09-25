16f2b40e85ed11aa3fb73128d4fccbeb2406ea9e
unknown <mthlima@poli.ufrj.br>
2020-09-25 10:46:56 -0300

    Plot predicted clusters

--- source/kmeans.py
+++ source/kmeans.py
@@ -18,3 +18,12 @@ centroids = cluster.cluster_centers_
 predictions = cluster.labels_
 
 confusion = confusion_matrix(iris.target, predictions)
+
+# Plot predicted clusters
+plt.scatter(iris.data[predictions == 0, 0], iris.data[predictions == 0, 1], 
+            c = 'green', label = 'Setosa')
+plt.scatter(iris.data[predictions == 1, 0], iris.data[predictions == 1, 1], 
+            c = 'red', label = 'Versicolor')
+plt.scatter(iris.data[predictions == 2, 0], iris.data[predictions == 2, 1], 
+            c = 'blue', label = 'Virgica')
+plt.legend()
