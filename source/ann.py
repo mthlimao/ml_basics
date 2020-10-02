516d86b0cf80ae70ba48d9c188cc9675b075643c
unknown <mthlima@poli.ufrj.br>
2020-10-02 15:39:11 -0300

    Define hyperparameter to be optimized (number of epochs), estimator, pipeline and grid

--- source/ann.py
+++ source/ann.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 import pandas
 from keras.models import Sequential
 from keras.layers import Dense
@@ -8,6 +8,8 @@ from sklearn import datasets
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
 from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
 
 # Fix random seed for reproducibility
@@ -26,3 +28,19 @@ encoded_Y = encoder.transform(Y)
 
 # Convert integers to dummy variables (i.e. one hot encoded)
 dummy_y = np_utils.to_categorical(encoded_Y)
+
+# Define model
+def simple_model():
+    model = Sequential()
+    model.add(Dense(4, input_dim=4, activation='relu'))
+    model.add(Dense(3, activation='sigmoid'))
+    
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+# Define hyperparameter to be optimized, estimator, pipeline and grid
+epochs = np.array([150, 250, 500])
+
+estimator = KerasClassifier(build_fn=simple_model, batch_size=5, verbose=0)
+pipeline = Pipeline([('standardize', StandardScaler()), ('mlp', estimator)])
+param_grid = dict(mlp__epochs=epochs)
